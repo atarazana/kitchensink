a358a8be641060cf9e7c3ac7dbebd90385072301
cvicens <carlos.vicens.alonso@gmail.com>
2023-04-10 11:52:27 +0200

    staging

--- src/main/java/org/jboss/as/quickstarts/kitchensink/rest/MemberResourceRESTService.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/rest/MemberResourceRESTService.java
@@ -23,7 +23,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.RequestScoped;
