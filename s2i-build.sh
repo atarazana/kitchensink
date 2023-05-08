24e5fc7c225d35952af82e7309aa0762cab30f7a
cvicens <carlos.vicens.alonso@gmail.com>
2023-05-08 19:52:07 +0200

    kind of working

--- s2i-build.sh
+++ s2i-build.sh
@@ -8,7 +8,10 @@
 # sudo podman login -u cvicensa@redhat.com -p \!N0str0m075. registry.redhat.io
 # sudo podman pull registry.redhat.io/jboss-eap-7/eap72-openshift:latest
 
-sudo /var/home/cvicensa/bin/s2i build . registry.redhat.io/jboss-eap-7/eap72-openshift:1.2 \
+sudo rm -rf upload
+
+sudo /var/home/cvicensa/bin/s2i build -c . registry.redhat.io/jboss-eap-7/eap72-openshift:1.2 \
   --env CUSTOM_INSTALL_DIRECTORIES="extensions" \
   --env MAVEN_ARGS_APPEND="-Dcom.redhat.xpaas.repo.jbossorg" kitchensink:latest
+sudo podman tag docker.io/library/kitchensink:latest localhost/kitchensink:latest
 
