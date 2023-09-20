27c9c9e73252baa177fcfcb595a60bb19e02af03
cvicens <carlos.vicens.alonso@gmail.com>
2023-09-20 15:00:16 +0200

    fixing udi image

--- udi/image-env.sh
+++ udi/image-env.sh
@@ -2,7 +2,7 @@
 
 export BUILDER_IMAGE=registry.redhat.io/jboss-eap-7/eap-74-openjdk8-openshift-rhel8:7.4.11
 # export BUILDER_IMAGE=registry.redhat.io/jboss-eap-7/eap72-openshift
-export ARTIFACT_VERSION="7.4"
+export ARTIFACT_VERSION="7.4.11"
 export ARTIFACT_ID=udi
 export GIT_HASH=$(git rev-parse HEAD)
 
--- udi/image-run.sh
+++ udi/image-run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# export WILDFLY_S2I_OUTPUT_DIR=/s2i-output
+
 . ./image-env.sh
 
-podman run -it --rm -p 8080:8080 --name ${PROJECT_ID}-${ARTIFACT_ID} localhost/${PROJECT_ID}-${ARTIFACT_ID}:${ARTIFACT_VERSION}
+podman run -it --rm -p 8080:8080 -u 9999 \
+  -e S2I_DELETE_SOURCE=false \
+  -e S2I_DESTINATION_DIR=/projects/ \
+  -e CUSTOM_INSTALL_DIRECTORIES=extensions \
+  --name ${PROJECT_ID}-${ARTIFACT_ID} \
+  localhost/${PROJECT_ID}-${ARTIFACT_ID}:${ARTIFACT_VERSION} bash
